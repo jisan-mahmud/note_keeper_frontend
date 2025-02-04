cd7fd2146d505d4a4b185c443f593419ab1fe0fb
Jisan Mahmud <jisa.mahmud20@gmail.com>
2025-02-05 00:34:49 +0600

    design footer

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Navbar from '../component/Navbar';
+import Footer from '../component/Footer';
 
 const Layout = () => {
     return (
         <div>
-            <Navbar></Navbar>
-            <Outlet/>
+            <Navbar/>
+            <div style={{ minHeight: 'calc(100vh - 100px)' }}>
+                <Outlet />
+            </div>
+            <Footer/>
         </div>
     );
 };
