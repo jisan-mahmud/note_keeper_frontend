28a710d5113288a8a959f2b4edc310da412f0160
Jisan Mahmud <jisa.mahmud20@gmail.com>
2025-02-06 01:05:08 +0600

    some changes in signup component

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -47,32 +47,31 @@ const Signup = () => {
     }
 
     return (
-        <div className="flex justify-center items-center overflow-hidden w-full min-h-screen p-4">
-            <div className="flex flex-col sm:flex-row w-full sm:w-fit justify-center items-center">
-                <div className="bg-amber-200 p-6 sm:p-10 mx-4 sm:mx-10 rounded-3xl w-full max-w-md">
-                    <h2 className="text-2xl text-center">Sign Up</h2>
-                    <form onSubmit={handleForm} onChange= {handleFormInput} className="mt-6 sm:mt-10">
-                        <NameField firstNameRef={firstName} lastNameRef={lastName} />
-                        <EmailField emailRef={email} />
-                        <PasswordField passwordRef={password} fieldType={'Password'} />
-                        <PasswordField passwordRef={confirmPassword} fieldType={'Confirm Password'} />
-                        
-                        <div className="flex items-center gap-2 mb-6 text-red-600">
-                            {error && (
-                                <>
-                                    <MdError />
-                                    <span>{error}</span>
-                                </>
-                            )}
-                        </div>
-
-                        <Buttton buttonType='Signup' />
-                    </form>
-
-
-                </div>
-            </div>
+        <div className="flex justify-center items-center overflow-hidden w-full min-h-[calc(100vh-100px)] p-4 my-10 sm:my-0">
+        <div className="flex flex-col sm:flex-row justify-center items-center w-full sm:w-auto">
+          <div className="bg-amber-200 p-10 mx-4 sm:mx-10 rounded-3xl w-full max-w-md">
+            <h2 className="text-2xl text-center">Sign Up</h2>
+            <form onSubmit={handleForm} onChange={handleFormInput} className="mt-6 sm:mt-10">
+              <NameField firstNameRef={firstName} lastNameRef={lastName} />
+              <EmailField emailRef={email} />
+              <PasswordField passwordRef={password} fieldType={'Password'} />
+              <PasswordField passwordRef={confirmPassword} fieldType={'Confirm Password'} />
+      
+              <div className="flex items-center gap-2 mb-6 text-red-600">
+                {error && (
+                  <>
+                    <MdError />
+                    <span>{error}</span>
+                  </>
+                )}
+              </div>
+      
+              <Buttton buttonType="Signup" />
+            </form>
+          </div>
         </div>
+      </div>
+      
 
     );
 };
