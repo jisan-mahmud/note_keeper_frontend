5b9724428994c65fa80b27c5f3203eafdd65fbcd
Jisan Mahmud <jisa.mahmud20@gmail.com>
2025-02-08 00:37:06 +0600

    try to fix sidebar collaps problem

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,29 +6,27 @@ import { menuContext } from "../context/MenuContext";
 
 const Home = () => {
     const { isOpen, setIsOpen } = useContext(menuContext);
-    const [screenSize, setScreenSize] = useState(window.innerWidth);
 
     useEffect(() => {
         const handleResize = () => {
-            setScreenSize(window.innerWidth);
+            const newWidth = window.innerWidth;
+            const isMobileSize = newWidth <= 768;
+    
+            // Only update `isOpen` if the width crosses the mobile breakpoint
+            if (isMobileSize && isOpen) {
+                setIsOpen(false); // Collapse sidebar on mobile by default
+            } else if (!isMobileSize && !isOpen) {
+                setIsOpen(true); // Open sidebar on larger screens by default
+            }
         };
     
         window.addEventListener("resize", handleResize);
-
+    
         // Cleanup event listener on component unmount
         return () => {
             window.removeEventListener("resize", handleResize);
         };
-    }, [])
-
-
-    useEffect(() => {
-        if (screenSize <= 768) {
-            setIsOpen(false); 
-        } else {
-            setIsOpen(true); 
-        }
-    }, [screenSize, setIsOpen]);
+    }, [isOpen, setIsOpen]);
 
     return (
         <div className="flex relative">
@@ -36,7 +34,7 @@ const Home = () => {
                 initial={{ x: 0, opacity: 1 }}
                 animate={{ x: isOpen ? 0 : -200, opacity: isOpen ? 1 : 0 }}
                 transition={{ duration: 0.3, ease: "easeInOut" }}
-                className= {`sm:flex px-6 lg:px-10 flex-col justify-between bg-[#FAD0C4] min-h-[calc(100dvh-100px)] ${isOpen ? 'fixed md:relative w-9/12 md:w-2/9' : 'w-0 pl-0 pr-0 none'}`}
+                className= {`sm:flex px-6 lg:px-10 flex-col justify-between bg-[#FAD0C4] min-h-[calc(100dvh-100px)] ${isOpen ? 'w-9/12 md:w-2/9' : 'w-0 pl-0 pr-0 none'}`}
             >
                 <TagsSidebar />
             </motion.div>
