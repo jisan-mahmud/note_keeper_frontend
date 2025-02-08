21d23ff6671d19fda95d1aba2d69822c6e2db99e
Jisan Mahmud <jisa.mahmud20@gmail.com>
2025-02-08 11:37:36 +0600

    fix collaps problem

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <div className='bg-[#FF6A88] py-2 text-center text-lg'>
+        <div className='bg-[#FF6A88] relative z-30 py-2 text-center text-lg'>
             <span>Develop By <a className='text-green-900' href='#'>Jisan Mahmud</a></span>
         </div>
     );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,24 +1,34 @@
 import { motion } from "framer-motion";
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import TagsSidebar from "../component/TagsSidebar";
 import Notes from "../component/Notes";
 import { menuContext } from "../context/MenuContext";
 
 const Home = () => {
-    const { isOpen } = useContext(menuContext);
+    const { isOpen, setIsOpen } = useContext(menuContext);
+
+    const [screenWidth, setScreenWidth] = useState(window.innerWidth)
+
+    
+
+    useEffect(() => {
+        if(screenWidth <= 768){
+            setIsOpen(false)
+        }
+    }, [])
 
     return (
         <div className="flex overflow-hidden">
             {/* Sidebar */}
             <motion.div
-    initial={{ x: 0, opacity: 1 }}
-    animate={{ x: isOpen ? 0 : -200, opacity: isOpen ? 1 : 0 }}
-    transition={{ duration: 0.3, ease: "easeInOut" }}
-    className={`fixed md:relative top-3 left-0 md:top-0 z-10 bg-[#FAD0C4] min-h-screen h-full
-        ${isOpen ? "w-9/12 md:w-2/9" : "w-0"}`}
->
-    <TagsSidebar />
-</motion.div>
+            initial={{ x: 0, opacity: 1 }}
+            animate={{ x: isOpen ? 0 : -200, opacity: isOpen ? 1 : 0 }}
+            transition={{ duration: 0.3, ease: "easeInOut" }}
+            className={`fixed md:relative top-4 left-0 md:top-0 z-10 bg-[#FAD0C4] min-h-screen h-full
+                ${isOpen ? "w-9/12 md:w-2/9" : "w-0"}`}
+            >
+                <TagsSidebar />
+            </motion.div>
 
 
             {/* Notes Content */}
