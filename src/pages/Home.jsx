6142f7ca75c59eeab7ddddfa32d99c9253f73ae9
Jisan Mahmud <jisa.mahmud20@gmail.com>
2025-02-08 11:04:22 +0600

    check sidebar collaps problem

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,24 +1,29 @@
 import { motion } from "framer-motion";
-import { useContext, useEffect, useState } from "react";
+import { useContext } from "react";
 import TagsSidebar from "../component/TagsSidebar";
 import Notes from "../component/Notes";
 import { menuContext } from "../context/MenuContext";
 
 const Home = () => {
     const { isOpen } = useContext(menuContext);
-    
 
     return (
         <div className="flex overflow-hidden">
+            {/* Sidebar */}
             <motion.div
                 initial={{ x: 0, opacity: 1 }}
                 animate={{ x: isOpen ? 0 : -200, opacity: isOpen ? 1 : 0 }}
                 transition={{ duration: 0.3, ease: "easeInOut" }}
-                className={`sidebar bg-[#FAD0C4] min-h-[100dvh] ${isOpen ? 'w-9/12 md:w-2/9' : 'w-0'}`}
+                className={`left-0 top-0 -z-10 bg-[#FAD0C4] min-h-screen h-full
+                    ${isOpen ? "w-9/12 md:w-2/9" : "w-0"}`}
             >
                 <TagsSidebar />
             </motion.div>
-            <Notes />
+
+            {/* Notes Content */}
+            <div className={`flex-1 -z-20 transition-all duration-300 ${isOpen ? "ml-[9/12] md:ml-2/9" : "ml-0"}`}>
+                <Notes />
+            </div>
         </div>
     );
 };
