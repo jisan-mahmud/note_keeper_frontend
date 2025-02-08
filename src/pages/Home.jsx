e5e78bf89cb0160d9b30601d93c8233793a452a9
Jisan Mahmud <jisa.mahmud20@gmail.com>
2025-02-08 11:22:53 +0600

    try no fix collaps problem

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -11,14 +11,15 @@ const Home = () => {
         <div className="flex overflow-hidden">
             {/* Sidebar */}
             <motion.div
-                initial={{ x: 0, opacity: 1 }}
-                animate={{ x: isOpen ? 0 : -200, opacity: isOpen ? 1 : 0 }}
-                transition={{ duration: 0.3, ease: "easeInOut" }}
-                className={`fixed md:relative top-3 left-0 md:top-0 -z-10 bg-[#FAD0C4] min-h-screen h-full
-                    ${isOpen ? "w-9/12 md:w-2/9" : "w-0"}`}
-            >
-                <TagsSidebar />
-            </motion.div>
+    initial={{ x: 0, opacity: 1 }}
+    animate={{ x: isOpen ? 0 : -200, opacity: isOpen ? 1 : 0 }}
+    transition={{ duration: 0.3, ease: "easeInOut" }}
+    className={`fixed md:relative top-3 left-0 md:top-0 z-10 bg-[#FAD0C4] min-h-screen h-full
+        ${isOpen ? "w-9/12 md:w-2/9" : "w-0"}`}
+>
+    <TagsSidebar />
+</motion.div>
+
 
             {/* Notes Content */}
             <div className={`flex-1 -z-20 transition-all duration-300 ${isOpen ? "ml-[9/12] md:ml-2/9" : "ml-0"}`}>
