bf13c48d8af2d25e69c65c54ed229e25c78a6f75
Jisan Mahmud <jisa.mahmud20@gmail.com>
2025-03-10 03:01:20 +0600

    change baseurl

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export const auth = axios.create({
-    baseURL: 'http://127.0.0.1:8000/api/auth/',
+    baseURL: 'https://ecarebd.pythonanywhere.com/api/auth/',
     headers: {
         'Content-Type': 'application/json' 
     }
--- src/utils/notes.js
+++ src/utils/notes.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const notes = axios.create({
-    baseURL: 'http://127.0.0.1:8000/',
+    baseURL: 'https://ecarebd.pythonanywhere.com/',
     headers: {
         'Content-Type': 'application/json',
     }
